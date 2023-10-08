8ea449d33fe5a8f029988d3f0703a67a491d17e0
João Guilherme Leal Zimny <zimnyjoao@outlook.com>
2023-10-08 01:08:56 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// Ativar links do Menu
 const links = document.querySelectorAll('.header-menu a');
-
 function ativarLink(link) {
     const url = location.href; // URL da página atual 
     const href = link.href; // href do link
@@ -10,3 +10,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if(elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
